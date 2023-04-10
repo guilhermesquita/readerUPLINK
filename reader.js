a5e5d0cedc989a464ac7c9c4e5f8ac145287dbce
guilhermer_mesquita@hotmail.com <guilherme.mesquita@aquaapia.com.br>
2023-04-10 13:36:26 -0400

    removing spaces!

--- reader.js
+++ reader.js
@@ -1,44 +1,50 @@
 function messageDecripter(uplink) {
-   
-    const pulseCounterHexadecimal = uplink.slice(0,8)
-    console.log(pulseCounterHexadecimal)
+
+    const uplinkTreatment = uplink.replace(/\s/g, '') //Eliminação de todos espaços em brancos
+
+    //TRATAMENTOS DADOS DO CONTADOR DE PULSOS
+    const pulseCounterHexadecimal = uplinkTreatment.slice(0, 8)
     const pulseCounter = parseInt(pulseCounterHexadecimal, 16)
 
-    const infoHexadecimal = uplink.slice(8,10)
-    const info = parseInt(infoHexadecimal, 16).toString(2)
-    console.log(infoHexadecimal)
+    //TRATAMENTOS DADOS DE ALARMES
+    const infoHexadecimal = uplinkTreatment.trim().slice(8, 10)
+    const binary = parseInt(infoHexadecimal, 16).toString(2)
 
-    const bateryHexadecimal = uplink.slice(10,12)
+    const info = []
+
+    for (i in binary) {
+        if (binary[i] === '1') {
+            if (i == 0) {
+                info.push('Alarme de corte de cabo ativo')
+            }
+            if (i == 2) {
+                info.push('Alarme de corte de vazamento ativo')
+            }
+            if (i == 3) {
+                info.push('Alarme de ataque magnético')
+            }
+        }
+    }
+
+    //TRATAMENTOS DADOS DE BATERIA
+    const bateryHexadecimal = uplinkTreatment.trim().slice(10, 12)
     const batery = parseInt(bateryHexadecimal, 16)
 
-    const intervalHexadecimal = uplink.slice(12,16)
-    const interval = parseInt(intervalHexadecimal, 16)/2
+    //TRATAMENTOS DADOS DE INTERVALOS
+    const intervalHexadecimal = uplinkTreatment.trim().slice(12, 16)
+    const interval = parseInt(intervalHexadecimal, 16) / 2
 
     const payload = {
         pulse_counter: pulseCounter,
-        info: info,
-        batery: batery+"%",
-        interval: interval+'min'
+        info: info.join("; "),
+        batery: batery,
+        interval: interval
     }
 
     return payload
 
-  }
+}
 
 //teste com dado mockado
-let teste = new messageDecripter('000004AC09630078')
-console.log(teste)
-
-//  function messageDecripter(uplink) {
-//     const message = parseInt(uplink, 16)
-
-//     const payload = {
-//         pulse_counter: message
-//     }
-
-//     return payload
-
-//   }
-
-//  let teste = new messageDecripter('000004AC')
-//  console.log(teste)
+let numberPayload = new messageDecripter('00 00 04 AC 09 63 00 78')
+console.log(numberPayload)
